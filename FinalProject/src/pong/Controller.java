24483e7887e406ca61d9f27d05f8804a0c7af5fb
Johnathan Lee <jfl747076@gmail.com>
2018-11-17 16:37:31 -0600

    Add Vec2 class; Pong now moves and bounces left/right

--- FinalProject/src/pong/Controller.java
+++ FinalProject/src/pong/Controller.java
@@ -3,6 +3,7 @@ package pong;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.animation.TimelineBuilder;
+import javafx.scene.Node;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.AnchorPane;
@@ -14,6 +15,57 @@ import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Deque;
 import java.util.Stack;
+
+class Vec2 {
+  public Vec2(Vec2 rhs) {
+    this.x = rhs.x;
+    this.y = rhs.y;
+  }
+
+  public Vec2(double x, double y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  /**
+   * Get from anchor pane constraints
+   * @param node
+   */
+  public Vec2(Node node) {
+    this.x = AnchorPane.getLeftAnchor(node);
+    this.y = AnchorPane.getBottomAnchor(node);
+  }
+
+  /**
+   * Because for some reason Java won't let you overload. (Unless it's a string, but we don't talk about that)
+   * @param rhs Right side of the equation vec1 + vec2
+   */
+  public void add(Vec2 rhs) {
+    this.x += rhs.x;
+    this.y += rhs.y;
+
+  }
+
+  public void mult(double coeff) {
+    this.x *= coeff;
+    this.y *= coeff;
+  }
+
+  public void setConstraints(Node node) {
+    AnchorPane.setLeftAnchor(node, this.x);
+    AnchorPane.setBottomAnchor(node, this.y);
+  }
+
+  @Override
+  public String toString() {
+    return "Vec2{" +
+            "x=" + x +
+            ", y=" + y +
+            '}';
+  }
+
+  double x, y;
+}
 
 public class Controller {
   static final double maxX = (1600 * 0.75); // Game scene takes 75% of the stage
@@ -29,6 +81,9 @@ public class Controller {
   }
 
   Deque<State> stateStack = new ArrayDeque<>();
+  boolean p2IsBot = true;
+
+  int p1Score = 0, p2Score = 0;
 
   boolean[][] controlStates = {{false, false}, {false, false}}; // {paddle1: {leftCtrl, rightCtrl}, paddle2: ...
 
@@ -38,21 +93,25 @@ public class Controller {
   public Rectangle paddle2;
 
   double speedMult = 5.0;
+  Vec2 pongVeloc = new Vec2(-1,-0.25);
+
 
   public void movePaddle(Rectangle paddle, Dir dir) {
-    double xPos = AnchorPane.getLeftAnchor(paddle);
+    Vec2 pos = new Vec2(paddle);
     if (dir == Dir.Right) {
 
-      if (xPos < (maxX - paddleWidth))
-        AnchorPane.setLeftAnchor(paddle, xPos + 2 * speedMult);
+      if (pos.x < (maxX - paddleWidth))
+        pos.x += 2 * speedMult;
       else
-        AnchorPane.setLeftAnchor(paddle, maxX - paddleWidth);
-    } else {
-      if (xPos > 0)
-        AnchorPane.setLeftAnchor(paddle, xPos - 2 * speedMult);
+        pos.x = maxX - paddleWidth;
+    } else if (dir == Dir.Left){
+      if (pos.x > 0)
+        pos.x -= 2 * speedMult;
       else
-        AnchorPane.setLeftAnchor(paddle, 0.0);
+        pos.x = 0.0;
     }
+
+    pos.setConstraints(paddle);
   }
 
   public void setKeyStatesTo(boolean bool, KeyCode key) {
@@ -85,8 +144,10 @@ public class Controller {
       setKeyStatesTo(true, key.getCode());
 
       if (key.getCode() == KeyCode.ESCAPE) {
-        if (stateStack.getFirst() != State.Paused) stateStack.push(State.Paused);
-        else stateStack.pop();
+        if (stateStack.getFirst() != State.Paused) // Don't push infinite pauses onto the stack.
+          stateStack.push(State.Paused);
+        else
+          stateStack.pop();
       }
     });
   }
@@ -95,7 +156,33 @@ public class Controller {
 
   }
 
+
+  static Integer handleAIInterval = 0;
+  private void handleAI() {
+    // Only handle every 100 ticks
+    if(handleAIInterval++ < 5)
+      return;
+    else
+      handleAIInterval = 0;
+
+    boolean[] ctrls = controlStates[1]; // P2 is index 1
+    double xPos = AnchorPane.getLeftAnchor(paddle2),
+            pongXPos = AnchorPane.getLeftAnchor(pong);
+
+    if(pongXPos <  xPos) {
+      ctrls[0] = true;
+      ctrls[1] = false;
+    }
+    else {
+      ctrls[1] = true;
+      ctrls[0] = false;
+    }
+  }
+
   public void handlePlaying() {
+    if(p2IsBot)
+      handleAI();
+
     if (controlStates[0][0])
       movePaddle(paddle1, Dir.Left);
 
@@ -107,6 +194,26 @@ public class Controller {
 
     if (controlStates[1][0])
       movePaddle(paddle2, Dir.Left);
+
+    Vec2 pongPos = new Vec2(pong);
+
+    //System.out.println("From " + pongPos);
+
+    if(pongPos.x <= 1 || pongPos.x >= (maxX - 1.0))
+      pongVeloc.x *= -1;
+
+    if(pongPos.y >= 450) { // 
+
+    }
+
+    pongPos.add(pongVeloc);
+
+
+    pongPos.setConstraints(pong);
+
+    //System.out.println("To " + pongPos);
+
+
   }
 
   public void handleWin(State which) {
